f0f53a7679213f5e32ace0ac46910649f402cc5c
Eli Goldberg <goldbee2@wwu.edu>
2022-04-08 15:29:33 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,58 +7,71 @@ import Form from "./Form";
 import Canvas from "./canvas/Canvas";
 
 function App(props) {
-
   const [listOfEvents, setTLEvents] = useState(props.timelineEvents);
   const [currID, setID] = useState(0);
-  
-  const eventList = listOfEvents.sort((a, b)=>compareNumbers(a.year, b.year)).map(tlEvent=> (
-    <TimelineEvent
-      id={tlEvent.id}
-      key={tlEvent.id}
-      year={tlEvent.year}
-      description={tlEvent.description}
-      title={tlEvent.title}
-      deleteEvent={deleteEvent}
-      />));
-      
-  function compareNumbers(a, b){
-    return a-b;
+
+  const eventList = listOfEvents
+    .sort((a, b) => compareNumbers(a.year, b.year))
+    .map((tlEvent) => (
+      <TimelineEvent
+        id={tlEvent.id}
+        key={tlEvent.id}
+        year={tlEvent.year}
+        description={tlEvent.description}
+        title={tlEvent.title}
+        deleteEvent={deleteEvent}
+      />
+    ));
+
+  function compareNumbers(a, b) {
+    return a - b;
   }
 
   function addTLEvent(year, title, description) {
     setID(currID + 1);
-    const thisEvent = {id:currID.toString(), year:year, title:title, description:description};
+    const thisEvent = {
+      id: currID.toString(),
+      year: year,
+      title: title,
+      description: description,
+    };
     console.log(thisEvent);
     setTLEvents([...listOfEvents, thisEvent]);
   }
 
-
   function deleteEvent(id) {
-    const remainingEvents = listOfEvents.filter(item => item.id !== id);
+    const remainingEvents = listOfEvents.filter((item) => item.id !== id);
     setTLEvents(remainingEvents);
   }
 
-
-
   return (
     <div className="App">
-      <header className="App-header">Eli's timeline thingy</header>
       <main className="App-main">
         {/* <DatePanel props={timelineEvents}/> */}
         {/* FORM INSTEAD OF DATEPANEL??? */}
-        <div id="Date-Panel">
-          <div id="event-form">
-            <Form addTLEvent = {addTLEvent}/>
-          </div>
+        <div id="container-left">
+          <h1>Timelines</h1>
+          <div id="Date-Panel" className="panel">
+            <div id="event-form">
+              <Form addTLEvent={addTLEvent} />
+            </div>
 
-          <h2>EVENTS</h2>
-          <ul id="Event-List">{eventList}</ul>
+            <h2>EVENTS</h2>
+            <ul id="Event-List">{eventList}</ul>
+          </div>
+          <button className="submit-button">download</button>
         </div>
-        <div id="canvas-container">
-          <Canvas id="timeline-canvas" tlEvents={listOfEvents.sort((a, b)=>compareNumbers(a.year, b.year))} width={'1920px'} height={'1080px'}/>
+        <div id="canvas-container"  className="panel">
+          <Canvas
+            id="timeline-canvas"
+            tlEvents={listOfEvents.sort((a, b) =>
+              compareNumbers(a.year, b.year)
+            )}
+            width={"1920px"}
+            height={"1080px"}
+          />
         </div>
       </main>
-      <footer className="App-footer">By Eli Goldberg</footer>
     </div>
   );
 }
--- src/canvas/Canvas.js
+++ src/canvas/Canvas.js
@@ -8,19 +8,25 @@ function Canvas(props) {
   const tlEvents = props.tlEvents;
 
   const LINE_WIDTH = 6;
-  const TLEVENT_FILL_STYLE = "#c3d6c8";
-  const TLEVENT_TEXT_FILL = "#2f690e";
-  const TLEVENT_XPADDING = 16;
+  const TLEVENT_FILL_STYLE = "#f3f3f3";
+  const TLEVENT_TEXT_FILL = "#3465a4";
+  const TLEVENT_LINE_FILL = "#3465a4";
+  const TLEVENT_XPADDING = 24;
   const TLEVENT_YPADDING = 32;
 
-  const TL_PADDING_LENGTH = 72;
-  const CANVAS_PADDING_LENGTH = 72;
+  const TL_PADDING_LENGTH = 100;
+  const CANVAS_PADDING_LENGTH = 100;
 
-  const SHADOW_COLOR = "rgba(0, 0, 0, 0.08)";
+  const SHADOW_COLOR = "rgba(10, 10, 10, 0.08)";
+  const SHADOW_COLOR_DARK = "rgba(70, 70, 70, 0.2)";
 
-  const TEXT_LINE_LENGTH = 40;
-  const FONT_HEIGHT_IN_PIXELS = 16;
-  const FONT_WIDTH_IN_PIXELS = 8;
+  const TEXT_LINE_LENGTH = 50;
+  const FONT_HEIGHT_IN_PIXELS = 14;
+  const FONT_WIDTH_IN_PIXELS = 6;
+  const TEXT_LINE_HEIGHT = 2;
+
+  const TITLE_FONT = 'bold 14px Arial';
+  const BODY_FONT = 'normal 12px Arial';
 
   const [MIDPOINT, setMidpoint] = useState(400); // midpoint is middle of canvas, handle updates later
 
@@ -33,11 +39,29 @@ function Canvas(props) {
     ctx.stroke();
   }
 
+
+
+
+
   function renderTLEvent(ctx, x, y, tlEvent, direction) {
     const desc = splitText(tlEvent.description);
     renderTLMarker(ctx, x, y, tlEvent);
-    const height = renderTLRect(ctx, x, y, tlEvent, direction);
-    renderTLText(ctx, x, y, tlEvent, direction);
+    const dimensions = renderTLRect(ctx, x, y, tlEvent, direction);
+    const width = dimensions[0];
+    const height = dimensions[1];
+
+
+    var textX = x;
+    if(direction){
+      textX = x - width;
+    }else{
+      textX = x + TLEVENT_XPADDING*2
+    }
+
+    var textY = y-(height/2)+TLEVENT_YPADDING;
+
+    renderTLTitle(ctx, textX, textY, tlEvent, direction);
+    renderTLText(ctx, textX, textY+TEXT_LINE_HEIGHT*FONT_HEIGHT_IN_PIXELS, tlEvent, direction);
     // generate padding length from currentY to bottom of padding.
     renderTLPadding(ctx, x, currentY);
     // then, update currentY to bottom of padding.
@@ -45,9 +69,26 @@ function Canvas(props) {
     currentY += TL_PADDING_LENGTH;
   }
 
-  function renderTLMarker(ctx, x, y) {
+  function renderTLTitle(ctx, x, y, tlEvent){
+    const title = tlEvent.year + " - " + tlEvent.title;
+    ctx.font = TITLE_FONT;
     ctx.fillStyle = TLEVENT_TEXT_FILL;
-    ctx.strokeStyle = TLEVENT_TEXT_FILL;
+    ctx.fillText(title, x, y);
+    ctx.font = BODY_FONT;
+  }
+
+  function renderTLText(ctx, x, y, tlEvent) {
+    const desc = splitText(tlEvent.description);
+    ctx.fillStyle = TLEVENT_TEXT_FILL;
+    for (let i = 0; i < desc.length; i++) {
+      ctx.fillText(desc[i], x, y + i * (TEXT_LINE_HEIGHT * FONT_HEIGHT_IN_PIXELS));
+    }
+  }
+
+
+  function renderTLMarker(ctx, x, y) {
+    ctx.fillStyle = TLEVENT_LINE_FILL;
+    ctx.strokeStyle = TLEVENT_LINE_FILL;
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, 2 * Math.PI);
     ctx.stroke();
@@ -57,18 +98,28 @@ function Canvas(props) {
     ctx.fill();
   }
 
+
+
+
   function renderTLEndCap(ctx, x, y) {
-    ctx.fillStyle = TLEVENT_TEXT_FILL;
-    ctx.strokeStyle = TLEVENT_TEXT_FILL;
+    ctx.fillStyle = TLEVENT_LINE_FILL;
+    ctx.strokeStyle = TLEVENT_LINE_FILL;
     ctx.beginPath();
     ctx.arc(x, y, 4, 0, 2 * Math.PI);
     ctx.fill();
   }
 
+
+
+
   function renderTLRect(ctx, x, y, tlEvent, direction) {
+    ctx.shadowOffsetX = 8;
+    ctx.shadowOffsetY = 8;
+    ctx.shadowColor = SHADOW_COLOR;
+    ctx.shadowBlur = 20;
     const desc = splitText(tlEvent.description);
     console.log(desc);
-    const height = FONT_HEIGHT_IN_PIXELS * desc.length + 2 * TLEVENT_YPADDING;
+    const height = FONT_HEIGHT_IN_PIXELS*TEXT_LINE_HEIGHT * desc.length + 2 * TLEVENT_YPADDING;
     const width =
       FONT_WIDTH_IN_PIXELS * TEXT_LINE_LENGTH + 2 * TLEVENT_XPADDING;
     ctx.fillStyle = TLEVENT_FILL_STYLE;
@@ -80,16 +131,22 @@ function Canvas(props) {
     x -= (2 * direction - 1) * TLEVENT_XPADDING;
     y -= height / 2;
     ctx.fillRect(x, y, width, height);
-    return height;
+    
+
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowBlur = 6;
+    ctx.shadowColor = SHADOW_COLOR_DARK;
+    ctx.fillRect(x, y, width, height);
+    return [width, height];
   }
 
-  function renderTLText(ctx, x, y, tlEvent, direction) {
-    // const desc = splitText(tlEvent.description);
-    // ctx.fillStyle = TLEVENT_TEXT_FILL;
-    // for (let i = 0; i < desc.length; i++) {
-    //   ctx.fillText(desc[i], x, y + i * (1.2 * FONT_HEIGHT_IN_PIXELS));
-    // }
-  }
+
+
+  
+
+
+
 
   function splitText(text, lineLength = TEXT_LINE_LENGTH) {
     const numLines = Math.ceil(text.length / lineLength);
@@ -101,13 +158,17 @@ function Canvas(props) {
     return lines;
   }
 
+
+  
+
+
   function canvasContent(ctx) {
     
     ctx.shadowOffsetX = 8;
     ctx.shadowOffsetY = 8;
     ctx.shadowColor = SHADOW_COLOR;
     ctx.shadowBlur = 20;
-
+    ctx.lineWidth = 2.6;
 
     console.log("Rerendering canvas content...");
     ctx.strokeStyle = "#fff000";
@@ -132,6 +193,6 @@ function Canvas(props) {
     canvasContent(ctx);
   });
 
-  return <canvas width="800px" height="800px" ref={canvasRef} />;
+  return <canvas width="800px" height="1000px" ref={canvasRef} />;
 }
 export default Canvas;
